43b024c7aab471cb87316d24dcb8274a05f728d9
Agatsya2709 <136240238+Agatsya2709@users.noreply.github.com>
2026-06-22 03:58:08 +0530

    commit

--- practice4.js
+++ practice4.js
@@ -1,2 +1,32 @@
-const snapshot= new Date().getFullYear();
-console.log(` Copyright ${snapshot} all rights reserved`);
+const snapshot = new Date().getFullYear();
+let prevprice = null;
+
+const timeOnly = new Date().toTimeString().slice(0, 8);
+const name = "qtech.ai";
+const symbol = "🚀";
+
+console.log(`Name: ${name}`);
+
+function expression() {
+    let p = Number((Math.random() * 1000).toFixed(2));
+
+    if (prevprice === null) {
+        console.log(`Price: $${p}`);
+    } else if (p > prevprice) {
+        console.log(`Price: $${p} 🔺`);
+    } else if (p < prevprice) {
+        console.log(`Price: $${p} 🔻`);
+    } else {
+        console.log(`Price: $${p} ➡️`);
+    }
+
+    prevprice = p;
+}
+
+setInterval(() => {
+    expression();
+}, 1000);
+
+console.log(`Symbol: ${symbol}`);
+console.log(`Current time is ${timeOnly}`);
+console.log(`Year: ${snapshot}`);
