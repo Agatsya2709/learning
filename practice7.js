28d40f56e69eb3f7ba32b5146bfe5b997ba5bdb0
Agatsya2709 <136240238+Agatsya2709@users.noreply.github.com>
2026-06-26 01:10:37 +0530

    commit

--- practice7.js
+++ practice7.js
@@ -1,6 +1,16 @@
 async function fetchData() {
     try {
-        const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts');
+        const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: 'Holiday Nightmare',
+                body: 'when i was kidnapped in scotland',
+                userId: 1
+            })
+        });
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
@@ -18,7 +28,7 @@ try{
     console.error('Error:', error);
 }finally{
     console.log("Execution completed.");
-}//get for getting data from the api and post for sending data to the api
+}//GET for getting data from the api and post for sending data to the api
 // POST for posting or sending data to the api
 // PUT for updating existing data on the api
 // DELETE for deleting data from the api
